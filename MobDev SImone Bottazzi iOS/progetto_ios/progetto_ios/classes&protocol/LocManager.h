36d6802dcf7df65abe0728a15230fde0cfe2acc9
Simone Bottazzi <bottax@tommyMac.fritz.box>
2023-01-21 17:22:59 +0100

    added geolocation manager; also 'confirm alerts' were added

---
+++ MobDev SImone Bottazzi iOS/progetto_ios/progetto_ios/classes&protocol/LocManager.h
@@ -0,0 +1,16 @@
+//
+//  LocManager.h
+//  progetto_ios
+//
+//  Created by Simone Bottazzi on 21/01/23.
+//
+
+#import <CoreLocation/CoreLocation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocManager : CLLocationManager
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MobDev SImone Bottazzi iOS/progetto_ios/progetto_ios/classes&protocol/LocManager.m
@@ -0,0 +1,12 @@
+//
+//  LocManager.m
+//  progetto_ios
+//
+//  Created by Simone Bottazzi on 21/01/23.
+//
+
+#import "LocManager.h"
+
+@implementation LocManager
+
+@end
